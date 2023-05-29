0b60f63241efa27cded5c77111097e1c03f3e4db
dburnham1212 <dburnham1212@gmail.com>
2023-05-29 16:28:47 -0400

    updated assert equal to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🤩🤩🤩 Assertion Passed: " + actual +  " === " + expected);
+    console.log(`🤩🤩🤩 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("☠️☠️☠️ Assertion Failed: " + actual +  " !== " + expected);
+    console.log(`☠️☠️☠️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
