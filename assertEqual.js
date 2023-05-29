470f6f9e93f653f95924de09b11ca1cfe332d78b
dburnham1212 <dburnham1212@gmail.com>
2023-05-29 15:56:04 -0400

    assert equals added

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🤩🤩🤩 Assertion Passed: " + actual +  " === " + expected);
+  } else {
+    console.log("☠️☠️☠️ Assertion Failed: " + actual +  " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
