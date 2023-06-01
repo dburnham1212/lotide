29f73f6433e60fb5f21425c7dc4613d11c32630a
dburnham1212 <dburnham1212@gmail.com>
2023-06-01 14:56:49 -0400

    map.js complete

---
+++ map.js
@@ -0,0 +1,41 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🤩🤩🤩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☠️☠️☠️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {// check if the arrays are equal in length
+    return false;//if not return false
+  }
+  for (let i = 0; i < arr1.length; i++) {// cycle through values in array
+    if (arr1[i] !== arr2[i]) {// check if array values are equal
+      return false;// if not return false
+    }
+  }
+  return true;// if all values are declared equal return true
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+//TEST FUNCTIONS
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+
+let nums = [1, 2, 3, 4, 5];
+const results2 = map(nums, num => num * 2);
+assertArraysEqual(results2, [2, 4, 6, 8, 10]);
+
+let animals = ["dog", "cat", "bunny", "snake"];
+const results3 = map(animals, animal => "pet_" + animal);
+assertArraysEqual(results3, ["pet_dog", "pet_cat", "pet_bunny", "pet_snake"]);
