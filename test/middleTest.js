b321f48ce9213b2516d1e345557d3227ef0188e3
dburnham1212 <dburnham1212@gmail.com>
2023-06-05 14:15:02 -0400

    added testing file middleTest.js for middle.js using Mocha and Chai to create test cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,31 @@
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle.js");
 
+//TEST CODE USING MOCHA AND CHAI
+const assert = require('chai').assert;
 
-// TEST CODE
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
+describe("#testing middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  }); 
 
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2] rounded length / 2 = 1
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3] rounded length / 2 = 2;
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3] rounded length / 2 = 2
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4] rounded length / 2 = 3
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]); // => [2, 3] rounded length / 2 = 2
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); // => [3, 4] rounded length / 2 = 3
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
